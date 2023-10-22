67deb9f371b496906b14ab518b62e9e244f1a577
Computerboer <git@ikbenhenk.nl>
2023-10-22 14:37:44 +0200

    Fix: possible fix for iphone not showing dates

--- src/_services/auction.service.ts
+++ src/_services/auction.service.ts
@@ -24,14 +24,28 @@ export class AuctionService {
     return this._http.get(environment.apiUrl + 'v2/auction/' + countrycode).pipe(
       map((response: any) => {
         response as MapLocation[];
+
+        response.map((l: MapLocation) => { l.auctions.map(a => a.closingtime = this.convertDate(a.closingtime)) })
+        response.map((l: MapLocation) => { l.auctions.map(a => a.starttime = this.convertDate(a.starttime)) })
         response.map((l: MapLocation) => { l.auctions.map(a => a._startbeforetoday = a.starttime > new Date()) })
-        response.map((l: MapLocation) => { l.auctions.map(a => a.closingtime = new Date(a.closingtime)) })
         console.log(response);
         return response;
       })
       , catchError(this.handleError)
     )
   }
+
+  convertDate(datestring: string): Date {
+    let date = datestring.split(" ")[0];
+    let time = datestring.split(" ")[1];
+
+    let datearray = date.split("-")
+    let timearray = time.split(":")
+
+    return new Date(Number(datearray[0]), Number(datearray[1]), Number(datearray[2]), Number(timearray[0]), Number(timearray[1]))
+
+  }
+
 
   private handleError(error: Response) {
     if (error.status === 400) {
