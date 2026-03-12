eb0c9902bbd20b01f7264ae8f13f196ec79888d9
Computerboer <hallo@ikbenhenk.nl>
2026-03-12 22:57:29 +0100

    FIX: removed duplicate / from url

--- src/_environments/_environment.ts
+++ src/_environments/_environment.ts
@@ -2,7 +2,7 @@ export const _sharedEnvironment = {
   TWKimageserver: '', //https://img.twa.nl/
   TWKurl: 'https://www.troostwijkauctions.com/nl',
   OVMimageserver: 'https://onlineveilingmeester.nl/',
-  OVMurl: 'https://onlineveilingmeester.nl/',
+  OVMurl: 'https://onlineveilingmeester.nl',
   APimageserver: '',
-  APurl: 'https://www.auctionport.be/nl/'
+  APurl: 'https://www.auctionport.be/nl'
 };
